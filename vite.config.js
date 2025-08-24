8ea67b0c334274ec17add34e1ad45162e5d108a4
feuile <ryokitagawa2017@gmail.com>
2025-08-24 15:34:24 +0900

    configを修正

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'TransactionForm'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/${repoName}/`,
+  base: '/TransactionForm/',
   plugins: [react()],
 })
